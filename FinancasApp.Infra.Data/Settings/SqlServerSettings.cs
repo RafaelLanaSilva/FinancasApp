12e3fbe187d8437aa50b5a0b29d554a06c045bd6
RafaelLanaSilva <rafaellanas@gmail.com>
2024-09-23 11:42:38 -0300

    Alteração da string de conexão

--- FinancasApp.Infra.Data/Settings/SqlServerSettings.cs
+++ FinancasApp.Infra.Data/Settings/SqlServerSettings.cs
@@ -11,6 +11,8 @@ namespace FinancasApp.Infra.Data.Settings
     /// </summary>
     public class SqlServerSettings
     {
-        public static string ConnectionString => "Data Source=localhost,1433;User ID=sa;Password=Coti2024!;Encrypt=False";
+        public static string ConnectionString => "Data Source=cotisqlserver.database.windows.net;Initial Catalog=bdcoti;User ID=usercoti;Password=Coti2024!;Encrypt=False";
+
+        //public static string ConnectionString => "Data Source=localhost,1433;User ID=sa;Password=Coti2024!;Encrypt=False";
     }
 }
